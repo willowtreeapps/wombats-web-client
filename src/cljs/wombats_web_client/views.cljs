4b4f772a8b22ac15dab9aa468af88a42e3a50a2e
Emily Seibert <emily.seibert@willowtreeapps.com>
2016-07-18 13:48:53 -0400

    split views into panels directory

--- src/cljs/wombats_web_client/views.cljs
+++ src/cljs/wombats_web_client/views.cljs
@@ -1,51 +1,14 @@
 (ns wombats_web_client.views
     (:require [re-frame.core :as re-frame]
-              [re-com.core :as re-com]))
-
-
-;; home
-
-(defn home-title []
-  (let [name (re-frame/subscribe [:name])]
-    (fn []
-      [re-com/title
-       :label (str "Hello from " @name ". This is the Home Page.")
-       :level :level1])))
-
-(defn link-to-about-page []
-  [re-com/hyperlink-href
-   :label "go to About Page"
-   :href "#/about"])
-
-(defn home-panel []
-  [re-com/v-box
-   :gap "1em"
-   :children [[home-title] [link-to-about-page]]])
-
-
-;; about
-
-(defn about-title []
-  [re-com/title
-   :label "This is the About Page."
-   :level :level1])
-
-(defn link-to-home-page []
-  [re-com/hyperlink-href
-   :label "go to Home Page"
-   :href "#/"])
-
-(defn about-panel []
-  [re-com/v-box
-   :gap "1em"
-   :children [[about-title] [link-to-home-page]]])
-
+              [re-com.core :as re-com]
+              [wombats_web_client.panels.home :as home]
+              [wombats_web_client.panels.about :as about]))
 
 ;; main
 
 (defmulti panels identity)
-(defmethod panels :home-panel [] [home-panel])
-(defmethod panels :about-panel [] [about-panel])
+(defmethod panels :home-panel [] [home/home-panel])
+(defmethod panels :about-panel [] [about/about-panel])
 (defmethod panels :default [] [:div])
 
 (defn show-panel
