383614179a14e3fa98c5d555a6d6b17a775cd823
Emily Seibert <emily.seibert@willowtreeapps.com>
2017-01-31 08:17:35 -0500

    form styling

--- src/cljs/wombats_web_client/components/modals/add-wombat-modal.cljs
+++ src/cljs/wombats_web_client/components/modals/add-wombat-modal.cljs
@@ -1,7 +1,9 @@
 (ns wombats-web-client.components.modals.add-wombat-modal
   (:require [re-frame.core :as re-frame]
             [reagent.core :as reagent]
-            [wombats-web-client.events.user :refer [create-new-wombat]]))
+            [wombats-web-client.events.user :refer [create-new-wombat]]
+            [wombats-web-client.utils.forms :refer [text-input-with-label
+                                                    cancel-modal-input]]))
 
 (def wombat-name (reagent/atom ""))
 (def wombat-url (reagent/atom ""))
@@ -22,21 +24,18 @@
      [:div.title "ADD WOMBAT"]
      (if (not (nil? @error)) [:div @error])
      [:form
-      [:label {:for "wombat-name"} "Bot Name"]
-      [:input {:type "text"
-               :name "wombat-name"
-               :on-change #(reset! wombat-name (-> % .-target .-value))}]
-      [:label {:for "wombat-url"} "Wombat URL"]
-      [:input {:type "text"
-               :name "wombat-url"
-               :on-change #(reset! wombat-url (-> % .-target .-value))}]
-      [:input {:type "button"
-               :value "Cancel"
-               :on-click (fn [] (re-frame/dispatch [:set-modal nil]))}]
-      [:input {:type "button"
-               :value "Add"
-               :on-click #(create-new-wombat 
-                           @wombat-name 
-                           @wombat-url 
-                           callback-success 
-                           callback-error)}]]]))
+      [text-input-with-label {:name "wombat-name" 
+                              :label "Wombat Name"
+                              :local-state-value wombat-name}]
+      [text-input-with-label {:name "wombat-url"
+                              :label "Wombat URL"
+                              :local-state-value wombat-url}]
+      [:div.action-buttons
+       [cancel-modal-input]
+       [:input.modal-button {:type "button"
+                             :value "ADD"
+                             :on-click #(create-new-wombat 
+                                         @wombat-name 
+                                         @wombat-url 
+                                         callback-success 
+                                         callback-error)}]]]]))
