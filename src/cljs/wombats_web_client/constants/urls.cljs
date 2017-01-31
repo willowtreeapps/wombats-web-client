be92a991d5391c901bf318154326fc22ed234e7d
Emily Seibert <emily.seibert@willowtreeapps.com>
2017-01-31 12:52:39 -0500

    made url update

--- src/cljs/wombats_web_client/constants/urls.cljs
+++ src/cljs/wombats_web_client/constants/urls.cljs
@@ -1,13 +1,12 @@
 (ns wombats-web-client.constants.urls)
 
-(def base-ws-url "//54.145.152.66")
-(def base-api-url "http://dev.wombats.io")
+(def base-api-url "//dev.wombats.io")
 
 (def self-url (str base-api-url "/api/v1/self"))
 (def github-signout-url (str base-api-url "/api/v1/auth/github/signout"))
 (def github-signin-url (str base-api-url "/api/v1/auth/github/signin"))
 
-(def game-url (str "ws:" base-ws-url "/ws/game"))
+(def game-url (str "ws:" base-api-url "/ws/game"))
 
 (defn my-wombats-url
   [id]
