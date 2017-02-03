8f174272125d019804ef7d3abd3e49a18777b70f
Emily Seibert <emily.seibert@willowtreeapps.com>
2017-02-03 15:44:26 -0500

    game card applied to my games and open games, arena preview hard coded, freq hard coded

--- src/cljs/wombats_web_client/panels/available-games.cljs
+++ src/cljs/wombats_web_client/panels/available-games.cljs
@@ -1,36 +1,23 @@
 (ns wombats-web-client.panels.open-games
   (:require [re-frame.core :as re-frame]
-            [wombats-web-client.events.games :refer [get-open-games
-                                                     join-open-game]]
-            [wombats-web-client.components.modals.join-wombat-modal :refer [join-wombat-modal]]))
+            [wombats-web-client.events.games :refer [get-open-games]]
+            [wombat-web-client.components.cards.game :refer [game-card]]))
 
 ;; Open Games Panel
-;; #(join-open-game game-id (:id (first @wombats)) "white")
-
-(defn temp-open-join-game-modal [game-id]
-  (fn []
-    (re-frame/dispatch [:set-modal #(join-wombat-modal game-id)])))
 
 (defn temp-poll-button []
   [:input {:type "button"
            :value "GET GAMES"
            :onClick #(get-open-games)}])
 
-(defn temp-game-card [game]
-  (let [game-id (:game/id game)
-        wombats (re-frame/subscribe [:my-wombats])]
-    [:li {:key game-id}
-     [:div {:style {:color "white"}} (str game)]
-     [:input.simple-button {:type "button"
-                            :value "JOIN"
-                            :onClick (temp-open-join-game-modal game-id)}]]))
 
 (defn panel []
   (let [open-games (re-frame/subscribe [:open-games])]
-    [:div.open-games-panel
-     [:div (str "This is the Open Games page.")]
+    [:div.open-games
      [temp-poll-button]
-     [:ul.open-games-list (map temp-game-card @open-games)]]))
+     [:ul.open-games-list 
+      (doall (for [game @open-games]
+               ^{:key (:game/id game)} [game-card game true]))]]))
 
 (defn login-prompt []
   [:div (str "You must login to see open games.")])
