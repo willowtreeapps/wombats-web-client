42bff292d75e04a6231f8cb0b55c3fa5c084803a
Christian Paul Dehli <dehli@gatech.edu>
2017-02-06 20:45:49 -0500

    Improved performance of arena.

--- src/cljs/wombats_web_client/utils/canvas.cljs
+++ src/cljs/wombats_web_client/utils/canvas.cljs
@@ -13,26 +13,13 @@
 
 (defn draw-image
   "Draws an image at a certain coordinate"
-  ([canvas image x y width height] (draw-image canvas 
-                                               image
-                                               x
-                                               y
-                                               width
-                                               height
-                                               0))
-  ([canvas image x y width height rotation]
-   (let [rotationRadians (/ (* js/Math.PI rotation) 180)
-         ctx (context canvas)]
-     
-     (.translate ctx x y)
-     (.translate ctx (/ width 2) (/ height 2))
-     (.rotate ctx rotationRadians)
-     
-     (.drawImage (context canvas) image (/ width -2) (/ height -2) width height)
-     
-     (.rotate ctx (* -1 rotationRadians))
-     (.translate ctx (/ width -2) (/ height -2))
-     (.translate ctx (* -1  x) (* -1 y)))))
+  [canvas image x y width height]
+  (.drawImage (context canvas)
+              image
+              x
+              y
+              width
+              height))
 
 (defn width
   "Gets the width of a canvas element"
