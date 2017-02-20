fc6934544230a509b227ebf960ba31762597b9a8
Emily Seibert <emily.seibert@willowtreeapps.com>
2017-02-20 15:57:12 -0500

    adjust hp if nil

--- src/cljs/wombats_web_client/components/game_ranking.cljs
+++ src/cljs/wombats_web_client/components/game_ranking.cljs
@@ -7,25 +7,32 @@
    (<= 1 hp 29) "dying"
    :else "none"))
 
+(defn get-adjusted-hp [isStarting?]
+  (if isStarting? 100 0))
+
 (defn ranking-box
-  [game-id stats]
+  [game-id stats info]
   (fn []
-    [:div {:class-name "game-ranking-box"}
-     [:ul.list-wombat-status
-      (for [{:keys [wombat-name
-                    username
-                    score
-                    hp
-                    color]} @stats]
-        ^{:key username} [:li.wombat-status {:class (when (= hp 0) "disabled")}
-                          [:div.health-bar
-                           [:span.filling {:class (get-health-color hp)
-                                           :style {:width (str hp "%")}}]]
-                          [:div.img-wrapper
-                           [:img {:src (str "/images/wombat_" color "_right.png")}]]
-                          [:div.wombat-name wombat-name]
-                          [:div.username username]
-                          [:div.score score]])]]))
+    (let [game-status (:status @info)
+          isStarting? (or (= game-status :pending-open) (= game-status :pending-closed))]
+      [:div {:class-name "game-ranking-box"}
+       [:ul.list-wombat-status
+        (for [{:keys [wombat-name
+                      username
+                      score
+                      hp
+                      color]} @stats]
+          (let [hpNil? (nil? hp)
+                adjusted-hp-value (if hpNil? (get-adjusted-hp isStarting?) hp)]
+            ^{:key username} [:li.wombat-status {:class (when (= adjusted-hp-value 0) "disabled")}
+                              [:div.health-bar
+                               [:span.filling {:class (get-health-color adjusted-hp-value)
+                                               :style {:width (str adjusted-hp-value "%")}}]]
+                              [:div.img-wrapper
+                               [:img {:src (str "/images/wombat_" color "_right.png")}]]
+                              [:div.wombat-name wombat-name]
+                              [:div.username username]
+                              [:div.score score]]))]])))
 
 
 
