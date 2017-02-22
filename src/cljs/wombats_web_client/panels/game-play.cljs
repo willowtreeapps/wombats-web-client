332d18ca8ae4777d6bd28d7ccd5169301442dfdf
Christian Paul Dehli <dehli@gatech.edu>
2017-02-22 13:37:40 -0500

    Clear game info on component unmount.

--- src/cljs/wombats_web_client/panels/game-play.cljs
+++ src/cljs/wombats_web_client/panels/game-play.cljs
@@ -35,6 +35,7 @@
   (re-frame/dispatch [:game/update-frame nil])
   (re-frame/dispatch [:game/clear-chat-messages])
   (re-frame/dispatch [:game/stats-update {}])
+  (re-frame/dispatch [:game/info nil])
   (ws/send-message :leave-game {:game-id @game-id})
   (reset! game-id nil)
   (re-frame/dispatch [:set-modal nil]))
