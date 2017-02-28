73e79c4ae28fc6aeb37c8b0bae43dfe907a0d0a5
Christian Paul Dehli <dehli@gatech.edu>
2017-02-28 16:09:45 -0500

    Fixed typo.

--- src/cljs/wombats_web_client/utils/url.cljs
+++ src/cljs/wombats_web_client/utils/url.cljs
@@ -1,6 +1,6 @@
 (ns wombats-web-client.utils.url
   (:require [pushy.core :as pushy]
-            [wombats-web-client.routes :refer [history]][wombats-web-client.routes :refer [history]]))
+            [wombats-web-client.routes :refer [history]]))
 
 (defn strip-access-token
   "removes access token from query"
