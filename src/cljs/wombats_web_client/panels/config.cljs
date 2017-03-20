bb097391b24f5d138867e1932fb5b227b5900897
Emily Seibert <emily.seibert@willowtreeapps.com>
2017-03-20 15:23:47 -0400

    PR changes

--- src/cljs/wombats_web_client/panels/config.cljs
+++ src/cljs/wombats_web_client/panels/config.cljs
@@ -1,15 +1,17 @@
 (ns wombats-web-client.panels.config
   (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]
             [wombats-web-client.components.header :refer [header]]
             [wombats-web-client.components.arena-table
              :refer [arena-table]]
             [wombats-web-client.events.arenas :refer [get-arenas]]))
 
 (defn config []
-  (get-arenas)
-
   (let [arenas (re-frame/subscribe [:arenas])]
-    (fn []
-      [:div.config-panel
-       [header "ARENAS"]
-       [arena-table arenas]])))
+    (reagent/create-class
+     {:component-did-mount #(get-arenas)
+      :reagent-render
+      (fn []
+        [:div.config-panel
+         [header "ARENAS"]
+         [arena-table arenas]])})))
