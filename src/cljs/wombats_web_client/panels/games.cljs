6068030ec03c2e39c8800e85fface940281382c1
Christian Paul Dehli <dehli@gatech.edu>
2017-04-11 08:45:48 -0400

    Working on pagination

--- src/cljs/wombats_web_client/panels/games.cljs
+++ src/cljs/wombats_web_client/panels/games.cljs
@@ -8,7 +8,7 @@
                                                     get-closed-games
                                                     get-my-closed-games]]))
 
-;; Games Panel
+;; Constants
 
 (defonce empty-open-page
   "Sorry, there are no games to join at the moment.")
@@ -19,30 +19,41 @@
 (defonce empty-finished-page
   "No games have ended yet! Check back later.")
 
-(defn- open-game-polling
-  "Poll for newly created games every minute"
-  []
-  (js/setInterval get-all-games 60000))
+;; Helpers
 
-(defn tab-view-toggle [cmpnt-state]
-  (let [show-open (:show-open @cmpnt-state)]
-    [:div.tab-game-toggle
-     [:div.game-tab {:class (when show-open "active")
-                     :on-click #(swap! cmpnt-state assoc :show-open true)}
-      "OPEN"]
-     [:div.game-tab {:class (when-not show-open "active")
-                     :on-click #(swap! cmpnt-state assoc :show-open false)}
-      "FINISHED"]]))
+(defn- parse-query-params
+  [{:keys [closed page mine]}]
+  (let [page-parsed (js/parseInt page)]
+    {:page (if (js/isNaN page-parsed) 0 page-parsed)
+     :mine (some? mine)
+     :closed (some? closed)}))
 
-(defn my-game-toggle [cmpnt-state]
-  (let [current-state (:show-my-games @cmpnt-state)]
-    [:div.my-game-toggle-wrapper
-     {:on-click #(swap! cmpnt-state
-                        assoc
-                        :show-my-games
-                        (not current-state))}
-     [:div.checkbox {:class (when current-state "selected")}]
-     [:div.desc "SHOW MY GAMES"]]))
+;; Lifecycle Methods
+
+(defn- component-will-mount
+  "Poll for whichever games are in query-params"
+  [query-params]
+  (let [params (parse-query-params query-params)]
+    (js/setInterval get-all-games 60000)))
+
+;; Render Methods
+
+(defn tab-view-toggle [show-closed]
+  [:div.tab-game-toggle
+   [:div.game-tab {:class (when-not show-closed "active")
+                   :on-click #(prn "SHOW OPEN")}
+    "OPEN"]
+   [:div.game-tab {:class (when show-closed "active")
+                   :on-click #(prn "SHOW CLOSED")}
+    "FINISHED"]])
+
+(defn my-game-toggle [show-my-games]
+  [:div.my-game-toggle-wrapper
+   {:on-click #(prn "TOGGLE MY GAMES")}
+   [:div.checkbox {:class (when show-my-games "selected")}]
+   [:div.desc "SHOW MY GAMES"]])
+
+;; Helper Methods
 
 (defn get-sorted-games [{:keys [show-open show-my-games games current-user]}]
   (cond
@@ -80,72 +91,53 @@
                     github-username (:user/github-username user)]
                 (= github-username current-username))) players)))
 
-(defn- parse-query-params
-  [{:keys [closed page mine]}]
-  (let [page-parsed (js/parseInt page)]
-    {:page (if (js/isNaN page-parsed) 0 page-parsed)
-     :mine (some? mine)
-     :closed (some? closed)}))
+
 
 (defn main-panel [{:keys [cmpnt-state query-params]}]
-  (let [polling (open-game-polling)
-        current-user (re-frame/subscribe [:current-user])
+  (let [current-user (re-frame/subscribe [:current-user])
         games (re-frame/subscribe [:games/games])
         {page :page
-         mine :mine
-         closed :closed} (parse-query-params query-params)]
+         show-my-games :mine
+         show-closed :closed} (parse-query-params query-params)]
 
-    (js/console.log page mine closed)
+    (js/console.log @games)
 
-    (get-all-games)
+    ;; Based on the query parameters, start polling for games
+    [:div.games-panel
+     [:div.toggles
+      [tab-view-toggle show-closed]
+      [my-game-toggle show-my-games]]
+     [:div.games
+      (if true
+        [:ul.games-list
+         (for [game []]
+           (let [status (:game/status game)
+                 players (:game/players game)
+                 user-in-game (first
+                               (get-user-in-game
+                                players
+                                current-user))
+                 is-joinable (and (= :pending-open status)
+                                  (nil? user-in-game))
+                 is-full (= :pending-closed status)
+                 is-playing (or (= :active status)
+                                (= :active-intermission status))
+                 num-joined (count players)]
 
-    (fn []
-      (let [current-user @current-user
-            show-my-games (:show-my-games @cmpnt-state)
-            show-open (:show-open @cmpnt-state)
-            games-sorted (get-sorted-games {:show-open show-open
-                                            :show-my-games show-my-games
-                                            :games @games
-                                            :current-user current-user})]
+             ^{:key (:game/id game)} [game-card game
+                                      user-in-game
+                                      is-joinable
+                                      is-full
+                                      is-playing
+                                      num-joined]))])
 
-        (swap! cmpnt-state assoc :polling polling)
-
-        [:div.games-panel
-         [:div.toggles
-          [tab-view-toggle cmpnt-state]
-          [my-game-toggle cmpnt-state]]
-         [:div.games
-          (if (pos? (count games-sorted))
-            [:ul.games-list
-             (for [game games-sorted]
-               (let [status (:game/status game)
-                     players (:game/players game)
-                     user-in-game (first
-                                   (get-user-in-game
-                                    players
-                                    current-user))
-                     is-joinable (and (= :pending-open status)
-                                      (nil? user-in-game))
-                     is-full (= :pending-closed status)
-                     is-playing (or (= :active status)
-                                    (= :active-intermission status))
-                     num-joined (count players)]
-
-                 ^{:key (:game/id game)} [game-card game
-                                          user-in-game
-                                          is-joinable
-                                          is-full
-                                          is-playing
-                                          num-joined]))]
-
-            [get-empty-state show-open show-my-games])]]))))
+      [get-empty-state nil nil]]]))
 
 (defn games [query-params]
-  (let [cmpnt-state (reagent/atom {:show-open true
-                                   :show-my-games false
-                                   :polling nil})]
+  (let [cmpnt-state (reagent/atom {:polling nil})]
     (reagent/create-class
-     {:component-will-unmount #(js/clearInterval (:polling @cmpnt-state))
+     {:component-will-mount #(component-will-mount query-params)
+      :component-will-unmount #(js/clearInterval (:polling @cmpnt-state))
       :reagent-render
       (fn [query-params]
         [main-panel {:cmpnt-state cmpnt-state
