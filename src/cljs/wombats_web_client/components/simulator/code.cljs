d4fe0324244969f0a457ad6620586d092fc22fff
Eli Bosley <elibosley@users.noreply.github.com>
2017-06-20 15:32:53 -0400

    Feature/output formatting  (#334)
    * Using ace to display JSON output from the simulator
    
    * Ace is now a component

--- src/cljs/wombats_web_client/components/simulator/code.cljs
+++ src/cljs/wombats_web_client/components/simulator/code.cljs
@@ -1,36 +1,17 @@
 (ns wombats-web-client.components.simulator.code
   (:require [reagent.core :as reagent]
-            [re-frame.core :as re-frame]))
+            [re-frame.core :as re-frame]
+            [wombats-web-client.components.ace :refer [ace-component]]))
 
 (defn- on-code-change! [editor]
   (fn []
     ;; Propogate the updated code into db
     (re-frame/dispatch [:simulator/update-code (.getValue editor)])))
 
-(defn- render-editor
-  [code]
-  [:div#editor])
-
-(defn- init-ace [code mode]
-  (let [editor (-> js/window
-                   .-ace
-                   (.edit "editor"))]
-
-    (when @code (-> editor
-                    .getSession
-                    (.setValue @code)))
-
-    (when @code (.on editor "change" (on-code-change! editor)))
-
-    (when @mode (-> editor
-                    .getSession
-                    (.setMode (str "ace/mode/" @mode))))
-
-    (.setTheme editor "ace/theme/tomorrow_night_eighties")))
-
 (defn render []
   (let [code (re-frame/subscribe [:simulator/code])
         mode (re-frame/subscribe [:simulator/code-mode])]
-    (reagent/create-class
-     {:reagent-render #(render-editor code)
-      :component-did-mount #(init-ace code mode)})))
+    [ace-component {:code @code
+                    :mode @mode
+                    :id "editor"
+                    :event-handler on-code-change!}]))
