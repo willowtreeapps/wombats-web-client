9c9bc78beb527ad893e0b208899ef1208b6e6394
Eli Bosley <eli.bosley@willowtreeapps.com>
2017-06-20 13:54:41 -0400

    Datepicker defaults to the current date and time

--- src/cljs/wombats_web_client/components/datepicker.cljs
+++ src/cljs/wombats_web_client/components/datepicker.cljs
@@ -2,7 +2,9 @@
   (:require [re-frame.core :as re-frame]
             [wombats-web-client.utils.forms :refer [get-value]]
             [wombats-web-client.utils.errors :refer [required-field-error]]
-            [wombats-web-client.components.inline-error :refer [inline-error]]))
+            [wombats-web-client.components.inline-error :refer [inline-error]]
+            [cljs-time.core :as t]
+            [cljs-time.format :as f]))
 
 (defn check-for-valid-date [state error-key value]
   (when (clojure.string/blank? value)
@@ -11,16 +13,27 @@
 (defn on-date-change [state key error-key value]
   (swap! state assoc error-key nil key value))
 
+(def time-formatter
+  (f/formatter "YYYY-MM-dTHH:mm"))
+
+(defn format-time [time-object]
+  (f/unparse time-formatter
+             time-object))
+
 (defn datepicker
   [{:keys [class name label state]}]
   (let [name-key (keyword name)
         error-key (keyword (str name "-error"))
         val (name-key @state)
-        error-val (error-key @state)]
+        error-val (error-key @state)
+        current-time (t/local-date-time (t/now))]
+    (println (format-time current-time))
+    (println "2015-01-02T11:42")
     [:div.datepicker-wrapper {:class class}
      [:label.label {:for name} label]
      [:input.input {:class (when error-val "field-error")
                     :type "datetime-local"
+                    :defaultValue (format-time current-time)
                     :on-blur #(check-for-valid-date
                                state
                                error-key
