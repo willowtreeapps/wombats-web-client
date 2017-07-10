43fe56400ed050ba397b9115da3fcab0a9fa766b
Eli Bosley <eli.bosley@willowtreeapps.com>
2017-07-10 14:43:14 -0400

    Updated split pane for simplification

--- src/cljs/wombats_web_client/components/simulator/split_pane.cljs
+++ src/cljs/wombats_web_client/components/simulator/split_pane.cljs
@@ -6,44 +6,34 @@
 
 
 (defonce navbar-height 45)
+(defonce max-height 105)
+(defonce top-size-px (reagent/atom 145))
 
-(defn get-client-rect [evt]
-  (let [r (.getBoundingClientRect (.-target evt))]
-    {:left (.-left r), :top (.-top r)}))
-
-(def top-size-px (reagent/atom 145))
-
-(defn mouse-move-handler [offset]
+(defn- mouse-move-handler [offset]
   (fn [evt]
-    (let [x (- (.-clientX evt) (:x offset))
-          y (- (.-clientY evt) (:y offset))
-          max-height 105]
+    (let [y (- (.-clientY evt) (:y offset))]
       (if (> y (- js/innerHeight max-height))
         (reset! top-size-px (- js/innerHeight max-height))
         (reset! top-size-px y)))))
 
 
-(defn mouse-up-handler [on-move]
+(defn- mouse-up-handler [on-move]
   (fn me [evt]
     (events/unlisten js/window EventType.MOUSEMOVE
                      on-move)))
 
-(defn mouse-down-handler [e]
-  (let [{:keys [left top]} (get-client-rect e)
-        offset             {:x (- (.-clientX e) left)
-                            :y (+ (- (.-clientY e) top) navbar-height)}
+(defn- mouse-down-handler [e]
+  (let [offset             {:y (+ 0 navbar-height)}
         on-move            (mouse-move-handler offset)]
     (events/listen js/window EventType.MOUSEMOVE
                    on-move)
     (events/listen js/window EventType.MOUSEUP
                    (mouse-up-handler on-move))))
 
-
 (defn- render-divider []
   [:div {:on-mouse-down mouse-down-handler}
    [:hr.panel-divider]
    [:hr.panel-grabber]])
-
 
 (defn render [top bottom]
   [:div.split-panel
