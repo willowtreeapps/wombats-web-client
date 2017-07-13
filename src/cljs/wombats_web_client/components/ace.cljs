ee7556d892f4bab3ec4d8adc135aa3c845484185
Eli Bosley <eli.bosley@willowtreeapps.com>
2017-07-13 13:54:56 -0400

    more updates based on review

--- src/cljs/wombats_web_client/components/ace.cljs
+++ src/cljs/wombats_web_client/components/ace.cljs
@@ -1,12 +1,12 @@
 (ns wombats-web-client.components.ace
   (:require [reagent.core :as reagent]))
 
-(defn- init-ace [{:keys [code mode id options event-handler]} atom]
+(defn- init-ace [{:keys [code mode id options event-handler editor-atom]}]
   (let [editor (-> js/window
                    .-ace
                    (.edit id))]
     (set! (.-$blockScrolling editor) js/Infinity)
-    (reset! atom editor)
+    (reset! editor-atom editor)
     (when code (-> editor
                    .getSession
                    (.setValue code)))
@@ -26,11 +26,11 @@
   (let [ace-element @editor]
     (when ace-element
       (let [position (.getCursorPosition ace-element)
-            code (:code  (get next-props 1))]
+            code (:code (get next-props 1))]
         (when code
           (-> ace-element
               .getSession
-              (.setValue (:code (get next-props 1)))))
+              (.setValue code)))
         (.resize ace-element)
         (.moveCursorToPosition ace-element position)))))
 
@@ -38,12 +38,14 @@
   "Takes a settings map and creates an Ace component with the specified settings
   See wombats-web-client.components.simulator.code for an example."
   [settings]
-  (let [editor (reagent/atom nil)]
+  (let [editor-atom (reagent/atom nil)
+        new-settings (assoc settings :editor-atom editor-atom)]
        (reagent/create-class
         {:reagent-render
          (fn [{:keys [id]}]
            [:div {:id id}])
          :component-did-mount
-         #(init-ace settings editor)
+         #(init-ace new-settings)
          :component-will-receive-props
-         #(refresh-ace %2 editor)})))
+         (fn [_ next-props editor-atom]
+           (refresh-ace next-props (:editor-atom new-settings)))})))
